eed677fc628ea7df3d4a0c9ab01de6bdcc521cae
dsa04156 <dsa04156@naver.com>
2023-10-08 14:39:13 +0900

    플로이드

--- java/src/BOJ/BOJ_11404_플로이드.java
+++ java/src/BOJ/BOJ_11404_플로이드.java
@@ -1,6 +1,7 @@
+package BOJ;
 import java.io.*;
 import java.util.*;
-class Main {
+class BOJ_11404_플로이드 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
