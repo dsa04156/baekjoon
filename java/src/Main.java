0dbd1719c98831416e07585a2b6080eb8fbf281f
dsa04156 <dsa04156@naver.com>
2023-10-10 23:45:39 +0900

    algo

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-class Main {
+class Main { 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
