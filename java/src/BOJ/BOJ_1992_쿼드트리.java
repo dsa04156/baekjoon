323b5483d27a482cba57fef4a11689e13635f4e6
dsa04156 <dsa04156@naver.com>
2023-11-01 00:27:32 +0900

    stack 문제

--- java/src/BOJ/BOJ_1992_쿼드트리.java
+++ java/src/BOJ/BOJ_1992_쿼드트리.java
@@ -1,15 +1,10 @@
+package BOJ;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
-class Main {
+class BOJ_1992_쿼드트리 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
