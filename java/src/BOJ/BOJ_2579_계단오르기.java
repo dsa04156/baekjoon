5f82852077d278816015d0459a1c9677b21d9517
dsa04156 <dsa04156@naver.com>
2024-06-14 11:38:25 +0900

    알고 문제풀이

--- java/src/BOJ/BOJ_2579_계단오르기.java
+++ java/src/BOJ/BOJ_2579_계단오르기.java
@@ -1,8 +1,10 @@
+package BOJ;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class BOJ_2579_계단오르기 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
